Zen-Vora/gradle

Cache Artifact Transforms across projects
In many cases, projects are initialized with functionally identical artifact transform registrations.
While the registrations themselves may have different parameters, they are initialized in the same order
and have the same from/to attributes.

Similar to caching we do with variant selection, and caching already done for source artifact sets in the transform selector,
we can store the indices of input artifact transforms, effectively reusing transform selection results across projects.

This commit creates a cacheable representation of a set of transform registrations for a given project, and then uses that
as an input to the artifact transform selection process. This cacheable representation is mapped back to the concrete
transform registrations after results are loaded from the cache.

This results in a notable reduction of artifact selection time in projects that use artifact transforms heavily.

platforms/software/dependency-management/src/main/java/org/gradle/api/internal/artifacts/transform/DefaultVariantTransformRegistry.java
@@ -16,6 +16,7 @@
 
 package org.gradle.api.internal.artifacts.transform;
 
+import com.google.common.collect.ImmutableList;
 import org.gradle.api.Action;
 import org.gradle.api.NonExtensible;
 import org.gradle.api.artifacts.transform.TransformAction;
@@ -26,6 +27,7 @@ import org.gradle.api.attributes.AttributeContainer;
 import org.gradle.api.internal.artifacts.TransformRegistration;
 import org.gradle.api.internal.artifacts.VariantTransformRegistry;
 import org.gradle.api.internal.attributes.AttributeContainerInternal;
+import org.gradle.api.internal.attributes.AttributeSchemaServices;
 import org.gradle.api.internal.attributes.AttributesFactory;
 import org.gradle.internal.Cast;
 import org.gradle.internal.instantiation.InstantiationScheme;
@@ -35,12 +37,15 @@ import org.gradle.internal.logging.text.TreeFormatter;
 import org.gradle.internal.service.ServiceRegistry;
 
 import javax.annotation.Nullable;
+import javax.inject.Inject;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class DefaultVariantTransformRegistry implements VariantTransformRegistry {
     private final List<TransformRegistration> registrations = new ArrayList<>();
     private final AttributesFactory attributesFactory;
+    private final AttributeSchemaServices attributeSchemaServices;
     private final ServiceRegistry services;
     private final InstantiatorFactory instantiatorFactory;
     private final InstantiationScheme parametersInstantiationScheme;
@@ -48,12 +53,24 @@ public class DefaultVariantTransformRegistry implements VariantTransformRegistry
     @SuppressWarnings("unchecked")
     private final IsolationScheme<TransformAction<?>, TransformParameters> isolationScheme = new IsolationScheme<TransformAction<?>, TransformParameters>((Class)TransformAction.class, TransformParameters.class, TransformParameters.None.class);
 
-    public DefaultVariantTransformRegistry(InstantiatorFactory instantiatorFactory, AttributesFactory attributesFactory, ServiceRegistry services, TransformRegistrationFactory registrationFactory, InstantiationScheme parametersInstantiationScheme) {
+    private final ReentrantLock registrationSnapshotLock = new ReentrantLock();
+    private RegisteredTransforms registrationSnapshot;
+
+    @Inject
+    public DefaultVariantTransformRegistry(
+        InstantiatorFactory instantiatorFactory,
+        AttributesFactory attributesFactory,
+        AttributeSchemaServices attributeSchemaServices,
+        ServiceRegistry services,
+        TransformRegistrationFactory registrationFactory,
+        TransformParameterScheme transformParameterScheme
+    ) {
         this.instantiatorFactory = instantiatorFactory;
         this.attributesFactory = attributesFactory;
+        this.attributeSchemaServices = attributeSchemaServices;
         this.services = services;
         this.registrationFactory = registrationFactory;
-        this.parametersInstantiationScheme = parametersInstantiationScheme;
+        this.parametersInstantiationScheme = transformParameterScheme.getInstantiationScheme();
     }
 
     @Override
@@ -97,6 +114,14 @@ public class DefaultVariantTransformRegistry implements VariantTransformRegistry
             validateAttributes(registration);
             TransformRegistration finalizedRegistration = registrationFactory.create(registration.from.asImmutable(), registration.to.asImmutable(), actionType, parameterObject);
             registrations.add(finalizedRegistration);
+
+            registrationSnapshotLock.lock();
+            try {
+                // TODO: Warn/throw if registrationSnapshot is non-null and registrations have already been read.
+                registrationSnapshot = null;
+            } finally {
+                registrationSnapshotLock.unlock();
+            }
         } catch (Exception e) {
             TreeFormatter formatter = new TreeFormatter();
             formatter.node("Could not register artifact transform ");
@@ -129,8 +154,16 @@ public class DefaultVariantTransformRegistry implements VariantTransformRegistry
     }
 
     @Override
-    public List<TransformRegistration> getRegistrations() {
-        return registrations;
+    public RegisteredTransforms getRegistrations() {
+        registrationSnapshotLock.lock();
+        try {
+            if (registrationSnapshot == null) {
+                registrationSnapshot = attributeSchemaServices.getRegisteredTransforms(ImmutableList.copyOf(registrations));
+            }
+            return registrationSnapshot;
+        } finally {
+            registrationSnapshotLock.unlock();
+        }
     }
 
     public static abstract class RecordingRegistration {

ushatechmanish/gradle_official

platforms/software/dependency-management/src/main/java/org/gradle/api/internal/artifacts/transform/DefaultVariantTransformRegistry.java
@@ -16,7 +16,7 @@
 
 package org.gradle.api.internal.artifacts.transform;
 
-import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.ImmutableList;
 import org.gradle.api.Action;
 import org.gradle.api.NonExtensible;
 import org.gradle.api.artifacts.transform.TransformAction;
@@ -27,6 +27,7 @@ import org.gradle.api.internal.DocumentationRegistry;
 import org.gradle.api.internal.artifacts.TransformRegistration;
 import org.gradle.api.internal.artifacts.VariantTransformRegistry;
 import org.gradle.api.internal.attributes.AttributeContainerInternal;
+import org.gradle.api.internal.attributes.AttributeSchemaServices;
 import org.gradle.api.internal.attributes.AttributesFactory;
 import org.gradle.internal.Cast;
 import org.gradle.internal.instantiation.InstantiationScheme;
@@ -34,37 +35,46 @@ import org.gradle.internal.instantiation.InstantiatorFactory;
 import org.gradle.internal.isolated.IsolationScheme;
 import org.gradle.internal.logging.text.TreeFormatter;
 import org.gradle.internal.service.ServiceLookup;
+import org.gradle.internal.service.ServiceRegistry;
 
 import javax.annotation.Nullable;
 import javax.inject.Inject;
 import java.util.LinkedHashSet;
 import java.util.Set;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class DefaultVariantTransformRegistry implements VariantTransformRegistry {
     private final Set<TransformRegistration> registeredTransforms = new LinkedHashSet<>();
 
     private final AttributesFactory attributesFactory;
+    private final AttributeSchemaServices attributeSchemaServices;
     private final ServiceLookup services;
     private final InstantiatorFactory instantiatorFactory;
     private final InstantiationScheme parametersInstantiationScheme;
     private final TransformRegistrationFactory registrationFactory;
     @SuppressWarnings({"unchecked", "rawtypes"})
     private final IsolationScheme<TransformAction<?>, TransformParameters> isolationScheme = new IsolationScheme<TransformAction<?>, TransformParameters>((Class)TransformAction.class, TransformParameters.class, TransformParameters.None.class);
+
+    private final ReentrantLock registrationSnapshotLock = new ReentrantLock();
+    private RegisteredTransforms registrationSnapshot;
     private final DocumentationRegistry documentationRegistry;
 
+    @Inject
     public DefaultVariantTransformRegistry(
         InstantiatorFactory instantiatorFactory,
         AttributesFactory attributesFactory,
-        ServiceLookup services,
+        AttributeSchemaServices attributeSchemaServices,
+        ServiceRegistry services,
         TransformRegistrationFactory registrationFactory,
-        InstantiationScheme parametersInstantiationScheme,
+        TransformParameterScheme transformParameterScheme,
         DocumentationRegistry documentationRegistry
     ) {
         this.instantiatorFactory = instantiatorFactory;
         this.attributesFactory = attributesFactory;
+        this.attributeSchemaServices = attributeSchemaServices;
         this.services = services;
         this.registrationFactory = registrationFactory;
-        this.parametersInstantiationScheme = parametersInstantiationScheme;
+        this.parametersInstantiationScheme = transformParameterScheme.getInstantiationScheme();
         this.documentationRegistry = documentationRegistry;
     }
 
@@ -74,8 +84,16 @@ public class DefaultVariantTransformRegistry implements VariantTransformRegistry
     }
 
     @Override
-    public Set<TransformRegistration> getRegistrations() {
-        return ImmutableSet.copyOf(registeredTransforms);
+    public RegisteredTransforms getRegistrations() {
+        registrationSnapshotLock.lock();
+        try {
+            if (registrationSnapshot == null) {
+                registrationSnapshot = attributeSchemaServices.getRegisteredTransforms(ImmutableList.copyOf(registeredTransforms));
+            }
+            return registrationSnapshot;
+        } finally {
+            registrationSnapshotLock.unlock();
+        }
     }
 
     private <T extends TransformParameters> void doRegisterTransform(Class<? extends TransformAction<T>> actionType, Action<? super TransformSpec<T>> registrationAction) {
@@ -91,6 +109,14 @@ public class DefaultVariantTransformRegistry implements VariantTransformRegistry
 
             TransformRegistration finalizedRegistration = registrationFactory.create(registration.from.asImmutable(), registration.to.asImmutable(), actionType, parameterObject);
             registeredTransforms.add(finalizedRegistration);
+
+            registrationSnapshotLock.lock();
+            try {
+                // TODO: Warn/throw if registrationSnapshot is non-null and registrations have already been read.
+                registrationSnapshot = null;
+            } finally {
+                registrationSnapshotLock.unlock();
+            }
         } catch (Exception e) {
             throw new VariantTransformConfigurationException(buildFailureToRegisterMsg(registration, actionType), e, documentationRegistry);
         }