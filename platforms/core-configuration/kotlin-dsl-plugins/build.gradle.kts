bhupendra129/gradle

Migrate production code to JSpecify annotations

platforms/core-configuration/kotlin-dsl-plugins/build.gradle.kts
@@ -36,6 +36,7 @@ dependencies {
     implementation(libs.futureKotlin("sam-with-receiver"))
     implementation(libs.futureKotlin("assignment"))
 
+    implementation(libs.jspecify)
 
     testImplementation(projects.logging)
     testImplementation(testFixtures(projects.kotlinDsl))

ushatechmanish/gradle_official

platforms/core-configuration/kotlin-dsl-plugins/build.gradle.kts
@@ -27,6 +27,7 @@ dependencies {
 
     compileOnly(libs.slf4jApi)
     compileOnly(libs.inject)
+    compileOnly(libs.jspecify)
 
     api(libs.kotlinStdlib)
 